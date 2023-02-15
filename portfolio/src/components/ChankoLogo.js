56c0d1d15aee87e0f4d0832d7073fb23581c0363
ericchanko <ericchanko@gmail.com>
2023-02-15 13:20:29 -0800

    started dev

---
+++ portfolio/src/components/ChankoLogo.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export const ChankoLogo = (props) => {
+    return (
+        <Image src='/../assets/chanko-logo.png' alt="Chanko's logo" width={20} height={20}></Image>
+    )
+}
+
+export default ChankoLogo;
